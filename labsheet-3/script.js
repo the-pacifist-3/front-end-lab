5830a23082d4bdcd720d8d63bbb0a812507d5982
Rohith <rrohith958@gmail.com>
2023-03-07 15:19:02 +0530

    test

---
+++ labsheet-3/script.js
@@ -0,0 +1,7 @@
+const ageRange = document.getElementById('age-range');
+const emailGroup= document.getElementById('email-group');
+const websiteGroup = document.getElementById('website-group');
+
+ageRange.style.display='none';
+emailGroup.style.display='none';
+websiteGroup.style.display='none';
